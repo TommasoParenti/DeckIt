178f7e0653f5e9f2fa2717fac0f56981df4a4fdc
Tommaso-Parenti <t.parenti1@studenti.unipi.it>
2026-08-03 19:48:39 +0200

    feat: define routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { VocabularyComponent } from './page/vocabulary/vocabulary.component';
+import { CategoriesComponent } from './page/categories/categories.component';
+import { FlashcardsComponent } from './page/flashcards/flashcards.component';
+import { StatisticsComponent } from './page/statistics/statistics.component';
 
-export const routes: Routes = [];
+// 2. Colleghiamo le rotte ai componenti finti
+export const routes: Routes = [
+  { path: '', redirectTo: 'vocabulary', pathMatch: 'full' },
+  { path: 'vocabulary', component: VocabularyComponent },
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'flashcards', component: FlashcardsComponent },
+  { path: 'statistics', component: StatisticsComponent },
+  { path: '**', redirectTo: 'vocabulary' }
+];
