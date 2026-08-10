2f635c311d07a8a58f59a307c4337240e397bb7e
Tommaso-Parenti <t.parenti1@studenti.unipi.it>
2026-08-10 17:03:33 +0200

    feat: add onAuthStateChange check in AuthService

--- src/app/services/auth-service/auth.service.ts
+++ src/app/services/auth-service/auth.service.ts
@@ -1,10 +1,21 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { supabase } from '../../core/supabase.client';
+import { Router } from '@angular/router';
 
 const APP_EMAIL = 'tommaso10parenti@gmail.com';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+  private router = inject(Router);
+
+  constructor() {
+    supabase.auth.onAuthStateChange((event) => {
+      if (event === 'SIGNED_OUT') {
+        this.router.navigateByUrl('/login');
+      }
+    });
+  }
+
   login(password: string) {
     return supabase.auth.signInWithPassword({ email: APP_EMAIL, password });
   }
