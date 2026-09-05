a5a3539cc69fa57f2c978c9d3283d2d852b432a5
Tommaso-Parenti <t.parenti1@studenti.unipi.it>
2026-09-05 23:33:26 +0200

    feat: add vocabulary card component

---
+++ src/app/component/vocabulary-card/vocabulary-card.component.ts
@@ -0,0 +1,31 @@
+import { Component, computed, inject, input, model, output } from '@angular/core';
+import { CategoriesService } from '../../services/categories.service';
+
+@Component({
+  selector: 'app-vocabulary-card',
+  imports: [],
+  templateUrl: './vocabulary-card.component.html',
+  styleUrl: './vocabulary-card.component.scss'
+})
+export class VocabularyCardComponent {
+  private categoriesService = inject(CategoriesService);
+
+  category = input('');
+  word = input('');
+  translation = input('');
+  description = input('');
+  spelling = input<string[]>([]);
+  categoryColor = computed(() => { this.categoriesService.categories().find(c => c.name === this.category())?.color ?? '6c757d' })
+
+  isFavorite = model(false);
+  cardClick = output<void>();
+
+  toggleStar(event: Event): void {
+    event.stopPropagation();
+    this.isFavorite.set(!this.isFavorite());
+  }
+
+  onCardClick(): void {
+    this.cardClick.emit();
+  }
+}
